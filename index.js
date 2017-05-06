4ef38ed93aabd376beebc1638e98fd350b677655
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2017-05-06 18:38:32 +0800

    Add eslint and editorconfig

--- index.js
+++ index.js
@@ -1,56 +1,53 @@
-'use strict';
-
 var Transform = require('readable-stream/transform');
 var rs = require('replacestream');
 var istextorbinary = require('istextorbinary');
 var parse5 = require('parse5');
-module.exports = function(search, replacement, options) {
-  return new Transform({
-    objectMode: true,
-    transform: function(file, enc, callback) {
-      if (file.isNull()) {
-        return callback(null, file);
-      }
+module.exports = function (search, replacement, options) {
+    return new Transform({
+        objectMode: true,
+        transform: function (file, enc, callback) {
+            if (file.isNull()) {
+                return callback(null, file);
+            }
 
-      function doReplace() {
-        if (file.isStream()) {
-          file.contents = file.contents.pipe(rs(search, replacement));
-          return callback(null, file);
-        }
+            function doReplace() {
+                if (file.isStream()) {
+                    file.contents = file.contents.pipe(rs(search, replacement));
+                    return callback(null, file);
+                }
 
-        if (file.isBuffer()) {
-          if (search instanceof RegExp) {
-            file.contents = new Buffer(String(file.contents).replace(search, replacement));
-          }
-          else {
-            let htmlString = String(file.contents);
-            const cssExtenalLink = /(\s*<link.*(type="text\/css")+.*(href=(http[s]{0,1}:\/\/)|(\/\/)).*\n?)/mig;
-            htmlString = htmlString.replace(cssExtenalLink, '');
-            file.contents = new Buffer(htmlString);
-          }
-          return callback(null, file);
-        }
+                if (file.isBuffer()) {
+                    if (search instanceof RegExp) {
+                        file.contents = new Buffer(String(file.contents).replace(search, replacement));
+                    } else {
+                        var htmlString = file.contents.toString();
+                        var exCSSReg = /(\s*<link.*(type="text\/css")+.*(href=(http[s]{0,1}:\/\/)|(\/\/)).*\n?)/mig;
+                        htmlString = htmlString.replace(exCSSReg, '');
+                        file.contents = new Buffer(htmlString);
+                    }
+                    return callback(null, file);
+                }
 
-        callback(null, file);
-      }
+                callback(null, file);
+            }
 
-      if (options && options.skipBinary) {
-        istextorbinary.isText(file.path, file.contents, function(err, result) {
-          if (err) {
-            return callback(err, file);
-          }
+            if (options && options.skipBinary) {
+                istextorbinary.isText(file.path, file.contents, function (err, result) {
+                    if (err) {
+                        return callback(err, file);
+                    }
 
-          if (!result) {
-            callback(null, file);
-          } else {
+                    if (!result) {
+                        callback(null, file);
+                    } else {
+                        doReplace();
+                    }
+                });
+
+                return;
+            }
+
             doReplace();
-          }
-        });
-
-        return;
-      }
-
-      doReplace();
-    }
-  });
+        }
+    });
 };
